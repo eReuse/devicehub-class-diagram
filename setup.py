fb3bbaaff4485f3a463287688c2330d940b2ac68
bustawin <xavier@bustawin.com>
2016-04-01 20:08:28 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeviceHub-Class-Diagram',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/eReuse/devicehub-class-diagram',
+    license='AGPLv3 License',
+    author='eReuse team',
+    author_email='x.bustamante@ereuse.org',
+    description='A small utility to generate class diagrams from the resources of DeviceHub',
+    install_requires=[
+        'graphviz',
+        'DeviceHub'
+    ],
+    include_package_data=True
+)
